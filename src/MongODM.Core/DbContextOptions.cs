2fc0feb5a82e98209dd76dcffde1d0d7b80c08cf
Mirko Da Corte <mirko@etherna.com>
2020-05-09 20:36:11 +0200

    Simplified DbContextOptions configuration

--- src/MongODM.Core/DbContextOptions.cs
+++ src/MongODM.Core/DbContextOptions.cs
@@ -1,11 +1,13 @@
 ﻿using Digicando.MongODM.Serialization;
+using System.Linq;
 
 namespace Digicando.MongODM
 {
     public class DbContextOptions
     {
         public string ConnectionString { get; set; } = "mongodb://localhost/localDb";
-        public string DBName { get; set; } = "localDb";
+        public string DBName => ConnectionString.Split('?')[0]
+                                                .Split('/').Last();
         public DocumentVersion DocumentVersion { get; set; } = "1.0.0";
     }
 
