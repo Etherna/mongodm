c42443080027b98d0268011a021a65728499923e
Mirko Da Corte <mirko@etherna.io>
2020-07-20 03:05:41 +0200

    Added sample domains

---
+++ samples/AspNetCoreSample/Models/EntityModelBase.cs
@@ -0,0 +1,18 @@
+﻿using Etherna.MongODM.Models;
+using System;
+
+namespace Etherna.MongODM.AspNetCoreSample.Models
+{
+    public abstract class EntityModelBase<TKey> : ModelBase, IEntityModel<TKey>
+    {
+        protected EntityModelBase()
+        {
+            CreationDateTime = DateTime.Now;
+        }
+
+        public virtual TKey Id { get; protected set; }
+        public virtual DateTime CreationDateTime { get; protected set; }
+
+        public virtual void DisposeForDelete() { }
+    }
+}
