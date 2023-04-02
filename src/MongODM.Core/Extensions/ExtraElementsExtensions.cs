b1554a99effd095cd8652bc091f961724f816203
Mirko Da Corte <mirko@etherna.io>
2023-04-02 22:14:37 +0200

    add extra elements extension

---
+++ src/MongODM.Core/Extensions/ExtraElementsExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Etherna.MongODM.Core.Extensions
+{
+    public static class ExtraElementsExtensions
+    {
+        public static TValue? TryGetExtraElementValue<TValue>(this IDictionary<string, object>? extraElements, string key) =>
+            extraElements is not null &&
+                extraElements.TryGetValue(key, out var objValue) &&
+                objValue is TValue value ?
+            value :
+            default;
+    }
+}
